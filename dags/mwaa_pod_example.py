d61f5c558e9a19bf6e589f9188a74e88ef7d52b0
Prabakaran <dbalaaji@3c0630170c7b.ant.amazon.com>
2022-12-15 08:14:03 -0500

    eks

--- dags/mwaa_pod_example.py
+++ dags/mwaa_pod_example.py
@@ -4,6 +4,15 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
+from airflow.providers.amazon.aws.hooks.eks import ClusterStates, NodegroupStates
+from airflow.providers.amazon.aws.operators.eks import (
+    EKSCreateClusterOperator,
+    EKSCreateNodegroupOperator,
+    EKSDeleteClusterOperator,
+    EKSDeleteNodegroupOperator,
+    EKSPodOperator,
+)
+from airflow.providers.amazon.aws.sensors.eks import EKSClusterStateSensor, EKSNodegroupStateSensor
 
 default_args = {
     "owner": "aws",
@@ -17,21 +26,32 @@ dag = DAG("kubernetes_pod_example", default_args=default_args, schedule_interval
 # use a kube_config stored in s3 dags folder for now
 kube_config_path = "/opt/airflow/dags/repo/dags/kube_config.yaml"
 
-podRun = KubernetesPodOperator(
-    namespace="mwaa",
-    image="ubuntu:18.04",
-    cmds=["bash"],
-    arguments=["-c", "ls"],
-    labels={"foo": "bar"},
-    name="mwaa-pod-test",
-    task_id="pod-task",
-    get_logs=True,
-    dag=dag,
-    is_delete_operator_pod=False,
-    config_file=kube_config_path,
-    in_cluster=False,
-    cluster_context="mwaa", # Must match kubeconfig context
-)
+# podRun = KubernetesPodOperator(
+#     namespace="mwaa",
+#     image="ubuntu:18.04",
+#     cmds=["bash"],
+#     arguments=["-c", "ls"],
+#     labels={"foo": "bar"},
+#     name="mwaa-pod-test",
+#     task_id="pod-task",
+#     get_logs=True,
+#     dag=dag,
+#     is_delete_operator_pod=False,
+#     config_file=kube_config_path,
+#     in_cluster=False,
+#     cluster_context="mwaa", # Must match kubeconfig context
+# )
+
+ start_pod = EKSPodOperator(
+        task_id="run_pod",
+        cluster_name="managed-airflow-mwaa",
+        image="amazon/aws-cli:latest",
+        cmds=["sh", "-c", "ls"],
+        labels={"demo": "hello_world"},
+        get_logs=True,
+        # Delete the pod when it reaches its final state, or the execution is interrupted.
+        is_delete_operator_pod=True,
+    )
 
 this_will_skip = BashOperator(
     task_id='this_will_skip',
