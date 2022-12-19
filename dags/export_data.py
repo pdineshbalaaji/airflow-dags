3395668617351a109085a49ef5fdd44e682ec14b
Prabakaran <dbalaaji@3c0630170c7b.ant.amazon.com>
2022-12-19 18:58:20 -0500

    test

--- dags/export_data.py
+++ dags/export_data.py
@@ -97,7 +97,7 @@ OBJECTS_TO_EXPORT = [
 def stream_to_S3_fn(result, filename):
     print(os.getcwd())
     print(filename)
-    s3_file = os.getcwd() + filename+'.csv'
+    s3_file = os.getcwd() + '/'+filename+'.csv'
     # only get 10K rows at a time
     REC_COUNT = 5000
     outfileStr = ""
