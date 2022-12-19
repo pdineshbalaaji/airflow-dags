a5e668746601a60694fbad89954593664939d7b5
Prabakaran <dbalaaji@3c0630170c7b.ant.amazon.com>
2022-12-19 14:26:31 -0500

    test

--- dags/export_data.py
+++ dags/export_data.py
@@ -59,7 +59,7 @@ ti.pool_slots, ti.queued_by_job_id, ti.external_executor_id, null as trigger_id 
 
 LOG_SELECT = "select dttm, dag_id, task_id, event, execution_date, owner, extra from log"
 
-TASK_FAIL_SELECT = "select task_id, dag_id, execution_date, \
+TASK_FAIL_SELECT = "select task_id, dag_id, run_id, \
  start_date, end_date, duration from task_fail"
 
 # TASK_RESCHEDULE = "select task_id, dag_id, execution_date, \
