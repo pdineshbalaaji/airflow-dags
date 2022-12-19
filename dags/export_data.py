38137d6a7ffdd30f8a7b754fa7f3d57bea35830f
Prabakaran <dbalaaji@3c0630170c7b.ant.amazon.com>
2022-12-19 17:20:11 -0500

    test

--- dags/export_data.py
+++ dags/export_data.py
@@ -98,6 +98,8 @@ def stream_to_S3_fn(result, filename):
     # only get 10K rows at a time
     REC_COUNT = 5000
     outfileStr = ""
+    s3_hook = S3Hook()
+    s3_client = s3_hook.get_conn()
     # results = result.fetchall();
     # print('this is here!!', str(results))
     # # print('header', vars(results[0]).keys())
@@ -116,7 +118,7 @@ def stream_to_S3_fn(result, filename):
         w.writerows(chunk)
     #     write_io.write(f.getvalue().encode("utf8"))
     # write_io.close()
-    s3.put_object(Bucket=S3_BUCKET+'/'+S3_KEY, Key=filename+'.csv', Body=f.getvalue())
+    s3_client.put_object(Bucket=S3_BUCKET+'/'+S3_KEY, Key=filename+'.csv', Body=f.getvalue())
 # pause all active dags to have consistend and reliable copy of dag history exports
 
 
